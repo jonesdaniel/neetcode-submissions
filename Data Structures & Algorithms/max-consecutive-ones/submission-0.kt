908f031503c8a6f8812b0b1d363bf4a7b3347d32
Daniel Jones <jonesdaniel21@gmail.com>
2026-08-19 19:41:20 -0400

    Add: max-consecutive-ones - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun findMaxConsecutiveOnes(nums: IntArray): Int {
+        var maxOnes = 0
+        var count = 0
+
+        // nums = [1,1,0,1,1,1]
+
+        for (i in nums.indices) {
+            if (nums[i] == 1) {
+                count++
+            } else {
+                count = 0
+            }
+
+            if (count > maxOnes) {
+                maxOnes = count
+            } 
+        }
+
+        return maxOnes
+    }
+}
